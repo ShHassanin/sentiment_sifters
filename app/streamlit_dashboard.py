44a9454db58a604b886d8416c8f05279de626020
ShHassanin <74568413+ShHassanin@users.noreply.github.com>
2024-11-05 16:07:05 +0200

    Add files via upload

--- app/streamlit_dashboard.py
+++ app/streamlit_dashboard.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 
-st.set_page_config(page_icon = 'app/dashboard_icon.jpg',page_title= 'Dashboard  ',layout="wide")
+st.set_page_config(page_title= 'Dashboard  ')
 
 st.image("app/dashboard.png")
 
